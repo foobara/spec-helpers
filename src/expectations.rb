ca7f62cb84f126c8b8a0fdbc1fd2d19708d5c1ff
Miles Georgi <azimux@gmail.com>
2023-08-27 16:55:17 -0700

    Add is_expected_to rspec helper

--- src/expectations.rb
+++ src/expectations.rb
@@ -1,7 +1,12 @@
 module RspecHelpers
   module Expectations
-    def is_expected_to_raise(*)
-      expect { subject }.to raise_error(*)
+    def is_expected_to_raise(*, &)
+      # expect { subject }.to raise_error(*, &)
+      is_expected_to raise_error(*, &)
+    end
+
+    def is_expected_to(expectation)
+      expect { subject }.to expectation
     end
   end
 end
