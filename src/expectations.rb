c888b84f66e252e1a69a5c25e6ddd0fb899828f7
Miles Georgi <azimux@gmail.com>
2023-08-28 20:37:13 -0700

    Add is_expected_to_not_raise and is_expected_to_not

--- src/expectations.rb
+++ src/expectations.rb
@@ -1,12 +1,19 @@
 module RspecHelpers
   module Expectations
     def is_expected_to_raise(*, &)
-      # expect { subject }.to raise_error(*, &)
       is_expected_to raise_error(*, &)
+    end
+
+    def is_expected_to_not_raise(*, &)
+      is_expected_to_not raise_error(*, &)
     end
 
     def is_expected_to(expectation)
       expect { subject }.to expectation
+    end
+
+    def is_expected_to_not(expectation)
+      expect { subject }.to_not expectation
     end
   end
 end
