9908f1f8f65b015b8ac93d4c18c664a6481fbf70
Miles Georgi <azimux@gmail.com>
2024-01-12 01:32:03 +0000

    Delete failing "does something useful" spec example

--- spec/foobara/spec_helpers_spec.rb
+++ spec/foobara/spec_helpers_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Foobara::SpecHelpers do
   it "has a version number" do
     expect(Foobara::SpecHelpers::VERSION).to_not be_nil
   end
-
-  it "does something useful" do
-    expect(false).to be(true)
-  end
 end
