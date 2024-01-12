d3956c47317563cae1e60f5d1b475276199b31db
Miles Georgi <azimux@gmail.com>
2024-01-12 00:17:37 +0000

    Add RubyMine generated gem files

---
+++ lib/foobara/spec_helpers.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "spec_helpers/version"
+
+module Foobara
+  module SpecHelpers
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/foobara/spec_helpers/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Foobara
+  module SpecHelpers
+    VERSION = "0.1.0"
+  end
+end
