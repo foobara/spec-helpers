9aeec4560da9f894c3d2b477915811a0c03a0a3a
Miles Georgi <azimux@gmail.com>
2024-01-12 01:44:34 +0000

    Setup spec_helper with simplecov and other settings/deps

--- lib/foobara/spec_helpers.rb
+++ lib/foobara/spec_helpers.rb
@@ -2,7 +2,5 @@ require_relative "spec_helpers/version"
 
 module Foobara
   module SpecHelpers
-    class Error < StandardError; end
-    # Your code goes here...
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,36 @@
-require "foobara/spec_helpers"
+require "bundler/setup"
+
+require "pry"
+require "pry-byebug"
+require "rspec/its"
+require "simplecov"
+
+SimpleCov.start do
+  # enable_coverage :branch
+  minimum_coverage line: 100
+  # TODO: enable this? worth it to get to 100% branch coverage?
+  # minimum_coverage line: 100, branch: 100
+end
 
 RSpec.configure do |config|
+  config.filter_run_when_matching :focus
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
+  config.order = :defined
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  # config.raise_errors_for_deprecations!
 end
+
+require "foobara/spec_helpers"
+
+# TODO: replace this with Util.require_directory when Util is in its own repository.
+Dir["#{__dir__}/../src/**/*.rb"].each { |f| require f }
