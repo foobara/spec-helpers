ee5809a26ae9ee4bae8e29f77ef689192b399bec
Miles Georgi <azimux@gmail.com>
2024-01-16 18:34:41 -0800

    Disable 100% coverage requirement

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,8 @@ require "simplecov"
 
 SimpleCov.start do
   # enable_coverage :branch
-  minimum_coverage line: 100
+  # Disabled because this repository depends on util/meta
+  # minimum_coverage line: 100
   # TODO: enable this? worth it to get to 100% branch coverage?
   # minimum_coverage line: 100, branch: 100
 end
